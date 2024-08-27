5798e85a50bbed9d563d363f57ea11503dd46e40
Calvin <calvin@nathandigital.com>
2024-08-27 18:03:57 +0300

    Add CRUD operations to RolesController

--- src/roles/roles.controller.ts
+++ src/roles/roles.controller.ts
@@ -1,4 +1,34 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Get, Put, Delete, Body } from '@nestjs/common';
+import { RolesService } from './roles.service';
 
 @Controller('roles')
-export class RolesController {}
+export class RolesController {
+    constructor (
+        private readonly rolesService: RolesService
+    ) {}
+
+    @Post()
+    async addNewRole(@Body) {
+        
+    }
+
+    @Get()
+    async getRoles() {
+        return this.rolesService.getRoles();
+    }
+
+    @Get(':roleId')
+    async getRole() {
+        return this.rolesService.getRole();
+    }
+
+    @Put(':roleId')
+    async updateRole() {
+        return this.rolesService.updateRole();
+    }
+
+    @Delete(':roleId')
+    async deleteRole() {
+        return this.rolesService.deleteRole();
+    }
+}
